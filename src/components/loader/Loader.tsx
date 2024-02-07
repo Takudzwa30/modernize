7997e039e10eb55b5b60957957623c010ceb7530
Takudzwa Allen Mushai <takudzwamushai@gmail.com>
2024-02-07 09:12:48 +0400

    added Loader

---
+++ src/components/loader/Loader.tsx
@@ -0,0 +1,47 @@
+"use client";
+import React, { useState, useEffect } from "react";
+
+// Styles
+import Style from "./LoaderWrapper.module.css";
+
+type LoaderProps = {
+  children: React.ReactNode;
+};
+
+const Loader: React.FC<LoaderProps> = ({ children }) => {
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      if (loading) {
+        document.body.style.overflow = "hidden";
+      } else {
+        document.body.style.overflow = "unset";
+      }
+    }
+  }, [loading]);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+  }, []);
+
+  return (
+    <div className={Style.loaderWrapper}>
+      {loading && (
+        <div className={Style.loaderContainer}>
+          <LoaderComponent />
+        </div>
+      )}
+      <div className={loading ? Style.hidden : ""}>{children}</div>
+    </div>
+  );
+};
+
+const LoaderComponent: React.FC = () => {
+  return <div className={Style.loader}></div>;
+};
+
+export default Loader;
