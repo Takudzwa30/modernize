494a387d3a6b74fb61dc6ad5c7b63d725249b578
Takudzwa Allen Mushai <takudzwamushai@gmail.com>
2024-04-18 16:19:53 +0400

    fixed url

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export function storeData(data: object, table: string) {
   axios.post(
-    `https://react-native-course-3cceb-default-rtdb.firebaseio.com/${table}.json`,
+    `https://modernize-eb7ad-default-rtdb.firebaseio.com/${table}.json`,
     data
   );
 }
