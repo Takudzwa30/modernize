1d89fc7d7876df4982f1acadd8d0dff5bb176c8d
Takudzwa Allen Mushai <34072789+Takudzwa30@users.noreply.github.com>
2024-06-17 18:40:54 +0400

    added env file

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAnDJJC3MQXMnu_DQv_CURxexmlFkiL3Fs",
-  authDomain: "modernize-eb7ad.firebaseapp.com",
-  databaseURL: "https://modernize-eb7ad-default-rtdb.firebaseio.com",
-  projectId: "modernize-eb7ad",
-  storageBucket: "modernize-eb7ad.appspot.com",
-  messagingSenderId: "1019700971910",
-  appId: "1:1019700971910:web:249cc04986b8d74a30c407",
-  measurementId: "G-MV9WDFPW5J",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
