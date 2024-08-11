268a67873f5f758298347b39481a7cabad998e8e
Takudzwa Allen <takudzwamushai@gmail.com>
2024-08-11 10:56:34 +0400

    added reusable breadcrumb

--- src/app/(main)/add-product/AddProductView.tsx
+++ src/app/(main)/add-product/AddProductView.tsx
@@ -1,6 +1,11 @@
 // Styles
+import BreadCrumb from "@/components/ui/breadCrumb/BreadCrumb";
 import Style from "./AddProductView.module.css";
 
 export default function AddProductView() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <BreadCrumb buttonTitle="Back" />
+    </div>
+  );
 }
